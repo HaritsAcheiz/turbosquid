ac1110b9b7389de00777e81f0db6580cb1bd31b5
HaritsAcheiz <harits.muhammad.only@gmail.com>
2023-05-20 05:33:21 +0700

    work on detail format

--- scraper.py
+++ scraper.py
@@ -1,3 +1,5 @@
+import time
+
 import requests
 from selectolax.parser import HTMLParser
 from dataclasses import dataclass, asdict
@@ -5,12 +7,23 @@ import os
 import json
 import csv
 import creds
+import re
 
 @dataclass
 class Item:
-    opt1: str
-    opt2: str
-    opt3: str
+    model_name: str
+    product_id: str
+    publish_date: str
+    page_link: str
+    download_link: str
+    price: str
+    model_license: str
+    format: str
+    polygons: str
+    vertices: str
+    textures: str
+    materials: str
+    uv: str
 
 @dataclass
 class Scraper:
@@ -35,22 +48,56 @@ class Scraper:
             return "Error: " + str(e)
         return r
 
-    def parse(self, r):
-        locator = ''
-        opt1_loc = ''
-        opt2_loc = ''
-        opt3_loc = ''
+    def get_detail(self, r):
         tree = HTMLParser(r.text)
-        staging = tree.css(locator)
         items = []
-        for sub in staging:
-            try:
-                opt1 = sub.css_first(opt1_loc)
-                opt2 = sub.css_first(opt2_loc)
-                opt3 = sub.css_first(opt3_loc)
-                items.append(asdict(Item(opt1 = opt1, opt2=opt2, opt3=opt3)))
-            except:
-                continue
+        i_formats = []
+        model_name = tree.css_first('div#product-title > h1').text()
+        product_id = tree.css_first('span#ProductID').text()
+        publish_date = tree.css_first('span#FPDatePublished').text()
+        page_link = r.url
+        try:
+            download_link = ''
+        except:
+            download_link = 'Not Available'
+        price = tree.css_first('span.text-xl:nth-of-type(3)').text()
+        if price == '':
+            price = tree.css_first('span#product-price').text()
+        else:
+            pass
+        model_license = tree.css_first(r'div.flex.flex-row.px-4 > span > span:nth-of-type(1) > span').text()
+        formats = tree.css('div#formats-container > div')
+        for i_format in formats:
+            i_formats.append(re.sub(r'\s+',' ',i_format.text()))
+        print(i_formats)
+        format = ",".join(i_formats)
+        polygons = tree.css_first('span#FPSpec_polygons').text()
+        vertices = tree.css_first('span#FPSpec_vertices').text()
+        if tree.css_first('span#FPSpec_textures'):
+            textures = 'Yes'
+        else:
+            textures = 'No'
+        if tree.css_first('span#FPSpec_materials'):
+            materials = 'Yes'
+        else:
+            materials = 'No'
+        if tree.css_first('span#FPSpec_unwrapped_uv'):
+            uv = 'Yes'
+        else:
+            uv = 'No'
+        items.append(asdict(Item(model_name=model_name,
+                                 product_id=product_id,
+                                 publish_date=publish_date,
+                                 page_link=page_link,
+                                 download_link=download_link,
+                                 price=price,
+                                 model_license=model_license,
+                                 format=format,
+                                 polygons=polygons,
+                                 vertices=vertices,
+                                 textures=textures,
+                                 materials=materials,
+                                 uv=uv)))
         return items
 
     def get_detail_url(self,r):
@@ -121,8 +168,9 @@ class Scraper:
         search_term = input('What do you want to search? ')
         page_num = 1
         detail_urls = []
-        while 1:
-            print(f'Scraping page {page_num} ...')
+        trial = 1
+        while trial < 3:
+            print(f'Scraping page {page_num}...')
             if page_num == 1:
                 url = f'https://www.turbosquid.com/3d-model/{search_term}?page_size=500&sort_column=a7&sort_order=desc'
             else:
@@ -131,12 +179,33 @@ class Scraper:
                 r = self.fetch(url)
                 detail_urls.extend(self.get_detail_url(r))
                 page_num += 1
+                trial = 1
+                break #prototype
             except Exception as e:
                 print(e)
-                break
+                time.sleep(3)
+                print('try to reconnect')
+                trial += 1
         self.detail_to_csv(set(detail_urls), 'detail_result2.csv')
         print('Scrape detail url completed')
-
+        items = []
+        trial = 1
+        for detail_url in detail_urls[0:2]:
+            while trial < 3:
+                print('Get item details...')
+                try:
+                    r = self.fetch(detail_url)
+                    # print(r.text)
+                    items.append(self.get_detail(r))
+                    trial = 1
+                    break
+                except Exception as e:
+                    print(e)
+                    time.sleep(3)
+                    print('try to reconnect')
+                    trial += 1
+            print(f'add detail from {detail_url}')
+        self.to_csv(set(items), 'result.csv')
 
 if __name__ == '__main__':
     s=Scraper()
