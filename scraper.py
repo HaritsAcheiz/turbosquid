36b15a1f717aeda80269b4f771b43591b49a3e99
HaritsAcheiz <harits.muhammad.only@gmail.com>
2023-05-21 02:42:36 +0700

    script completely work

--- scraper.py
+++ scraper.py
@@ -1,5 +1,4 @@
 import time
-
 import requests
 from selectolax.parser import HTMLParser
 from dataclasses import dataclass, asdict
@@ -8,6 +7,8 @@ import json
 import csv
 import creds
 import re
+import asyncio
+import aiohttp
 
 @dataclass
 class Item:
@@ -48,31 +49,64 @@ class Scraper:
             return "Error: " + str(e)
         return r
 
+    async def fetch_detail(self, s, url):
+        proxies = f'http://{creds.proxy_username}:{creds.proxy_password}@{creds.proxy_url}:{creds.proxy_port}'
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:109.0) Gecko/20100101 Firefox/113.0'
+        }
+
+        async with s.get(url, headers=headers, proxy=proxies, timeout=7) as r:
+            if r.status != 200:
+                r.raise_for_status()
+
+            return await r.text()
+
+    async def fetch_all(self, s, urls):
+        tasks = list()
+        for url in urls:
+            task = asyncio.create_task(self.fetch_detail(s, url))
+            tasks.append(task)
+        res = await asyncio.gather(*tasks)
+        return res
+
+    async def run(self, urls):
+        session_timeout = aiohttp.ClientTimeout(total=None, sock_connect=7, sock_read=10)
+        async with aiohttp.ClientSession(timeout=session_timeout) as s:
+            htmls = await self.fetch_all(s, urls)
+        return htmls
+
     def get_detail(self, r):
         tree = HTMLParser(r.text)
-        items = []
-        i_formats = []
-        model_name = tree.css_first('div#product-title > h1').text()
-        product_id = tree.css_first('span#ProductID').text()
-        publish_date = tree.css_first('span#FPDatePublished').text()
+        model_name = tree.css_first('div#product-title > h1').text().strip()
+        product_id = re.findall('\d+',tree.css_first('span#ProductID').text())[0]
+        publish_date = tree.css_first('span#FPDatePublished').text().strip()
         page_link = r.url
         try:
             download_link = ''
         except:
             download_link = 'Not Available'
-        price = tree.css_first('span.text-xl:nth-of-type(3)').text()
+        price = tree.css_first('span.text-xl:nth-of-type(3)').text().strip()
         if price == '':
-            price = tree.css_first('span#product-price').text()
+            price = tree.css_first('span#product-price').text().strip()
         else:
             pass
-        model_license = tree.css_first(r'div.flex.flex-row.px-4 > span > span:nth-of-type(1) > span').text()
-        formats = tree.css('div#formats-container > div')
-        for i_format in formats:
-            i_formats.append(re.sub(r'\s+',' ',i_format.text()))
-        print(i_formats)
-        format = ",".join(i_formats)
-        polygons = tree.css_first('span#FPSpec_polygons').text()
-        vertices = tree.css_first('span#FPSpec_vertices').text()
+        model_license = tree.css_first(r'div.flex.flex-row.px-4 > span > span:nth-of-type(1) > span').text().strip()
+        stage1 = tree.css('div#formats-container > div')
+        sentences = []
+        for x in stage1[1:]:
+            stage2 = x.css('span')
+            for y in stage2:
+                char = re.sub('\n','',y.text().strip())
+                word = ''.join(char)
+                if word == ' ':
+                    word = ', '
+                else:
+                    pass
+                sentences.append(word)
+            for sentence in sentences:
+            format = ", ".join(sentences)
+        polygons = re.sub('\s+',' ',tree.css_first('span#FPSpec_polygons').text().strip())
+        vertices = re.sub('\s+',' ',tree.css_first('span#FPSpec_vertices').text().strip())
         if tree.css_first('span#FPSpec_textures'):
             textures = 'Yes'
         else:
@@ -85,7 +119,7 @@ class Scraper:
             uv = 'Yes'
         else:
             uv = 'No'
-        items.append(asdict(Item(model_name=model_name,
+        items = asdict(Item(model_name=model_name,
                                  product_id=product_id,
                                  publish_date=publish_date,
                                  page_link=page_link,
@@ -97,7 +131,7 @@ class Scraper:
                                  vertices=vertices,
                                  textures=textures,
                                  materials=materials,
-                                 uv=uv)))
+                                 uv=uv))
         return items
 
     def get_detail_url(self,r):
@@ -107,28 +141,14 @@ class Scraper:
         staging = tree.css(locator)
         detail_urls = []
         for sub in staging:
-            try:
-                detail_url = sub.css_first(detail_url_loc).attributes['data-link']
-                detail_urls.append(detail_url)
-            except Exception as e:
-                print(e)
-                continue
+            detail_url = sub.css_first(detail_url_loc).attributes['data-link']
+            detail_urls.append(detail_url)
         return detail_urls
-
-    def download_img(self, img_urls, folder_name):
-        for url in img_urls:
-            if not os.path.exists(folder_name):
-                os.mkdir(folder_name)
-            if url != None:
-                with requests.Session() as s:
-                    r = s.get(url)
-                with open(f"{folder_name}/{url.split('/')[-1]}", 'wb') as f:
-                    f.write(r.content)
-            print('Image downloaded successfully!')
 
     def to_csv(self, datas, filename, headers):
         try:
             for data in datas:
+                print(data)
                 try:
                     file_exists = os.path.isfile(filename)
                     with open(filename, 'a', encoding='utf-8') as f:
@@ -166,9 +186,11 @@ class Scraper:
 
     def main(self):
         search_term = input('What do you want to search? ')
-        page_num = 1
+        page_num = 44
         detail_urls = []
         trial = 1
+        headers = ['model_name', 'product_id', 'publish_date', 'page_link', 'download_link', 'price', 'model_license',
+                   'format', 'polygons', 'vertices', 'textures', 'materials','uv']
         while trial < 3:
             print(f'Scraping page {page_num}...')
             if page_num == 1:
@@ -177,25 +199,28 @@ class Scraper:
                 url = f'https://www.turbosquid.com/3d-model/{search_term}?page_num={page_num}&page_size=500&sort_column=a7&sort_order=desc'
             try:
                 r = self.fetch(url)
-                detail_urls.extend(self.get_detail_url(r))
-                page_num += 1
-                trial = 1
-                break #prototype
+                detail_url = self.get_detail_url(r)
+                if detail_url != []:
+                    detail_urls.extend(detail_url)
+                    page_num += 1
+                    trial = 1
+                else:
+                    break
             except Exception as e:
                 print(e)
                 time.sleep(3)
                 print('try to reconnect')
                 trial += 1
+
         self.detail_to_csv(set(detail_urls), 'detail_result2.csv')
         print('Scrape detail url completed')
         items = []
         trial = 1
-        for detail_url in detail_urls[0:2]:
+        for detail_url in set(detail_urls):
             while trial < 3:
                 print('Get item details...')
                 try:
                     r = self.fetch(detail_url)
-                    # print(r.text)
                     items.append(self.get_detail(r))
                     trial = 1
                     break
@@ -205,7 +230,19 @@ class Scraper:
                     print('try to reconnect')
                     trial += 1
             print(f'add detail from {detail_url}')
-        self.to_csv(set(items), 'result.csv')
+        self.to_csv(items, 'result.csv', headers=headers)
+
+        # responses = []
+        # trial = 1
+        # while trial < 3:
+        #     try:
+        #         responses = asyncio.run(self.run(set(detail_urls)))
+        #         break
+        #     except:
+        #         trial += 1
+
+        # items = [self.get_detail(response) for response in responses]
+        # self.to_csv(items, 'result.csv', headers=headers)
 
 if __name__ == '__main__':
     s=Scraper()
