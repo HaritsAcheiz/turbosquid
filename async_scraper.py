ca32decac185e37d9115f1b667f6d6daf9b2e66b
Muhammad Harits R <harits.muhammad.only@gmail.com>
2023-05-23 03:43:38 +0700

    structured async

--- async_scraper.py
+++ async_scraper.py
@@ -2,7 +2,11 @@ import httpx
 from selectolax.parser import HTMLParser
 import json
 import asyncio
-from dataclasses import dataclass
+from dataclasses import dataclass, asdict
+import re
+import csv
+import os
+import creds
 
 @dataclass
 class Item:
@@ -24,98 +28,158 @@ class Item:
 @dataclass
 class Scraper:
 
+    def get_page(self, keyword):
+        proxies = {
+            'http://': f'http://{creds.proxy_username}:{creds.proxy_password}@{creds.proxy_url}:{creds.proxy_port}',
+            'https://': f'http://{creds.proxy_username}:{creds.proxy_password}@{creds.proxy_url}:{creds.proxy_port}'
+        }
+        url = f'https://www.turbosquid.com/3d-model/{keyword}?page_size=500&sort_column=a7&sort_order=desc'
+        with httpx.Client() as client:
+            response = client.get(url)
+        tree = HTMLParser(response.text)
+        return tree.css_first('span#ts-total-pages').text()
+
 
     async def fetch_id(self, client, url):
+        response = await client.get(url, timeout=10.0)
+        return response.text
+
+
+    async def fetch_all_id(self, keyword, last_page):
+        proxies = {
+            'http://': f'http://{creds.proxy_username}:{creds.proxy_password}@{creds.proxy_url}:{creds.proxy_port}',
+            'https://': f'http://{creds.proxy_username}:{creds.proxy_password}@{creds.proxy_url}:{creds.proxy_port}'
+        }
+
+        urls = [f'https://www.turbosquid.com/3d-model/{keyword}?page_num={page_num}&page_size=500&sort_column=a7&sort_order=desc' for page_num in range(1, int(last_page)+1)]
+
+        async with httpx.AsyncClient(proxies=proxies) as client:
+            tasks = [asyncio.create_task(self.fetch_id(client,url)) for url in urls]
+            responses = await asyncio.gather(*tasks, return_exceptions=True)
+            return responses
+
+
+    def parse_id(self, responses):
         staging_loc = 'div#SearchResultAssets > div.search-lab.AssetTile-md.tile-large'
         detail_url_loc = 'div.AssetInner > div'
-        response = await client.get(url)
-        tree = HTMLParser(response.text)
-        staging = tree.css(staging_loc)
-        item_ids = [article.css_first(detail_url_loc).attributes['data-id'] for article in staging]
+        for response in responses:
+            tree = HTMLParser(response)
+            staging = tree.css(staging_loc)
+            item_ids = [article.css_first(detail_url_loc).attributes['data-id'] for article in staging]
         return item_ids
 
-    async def fetch_all_id(self):
-        async with httpx.AsyncClient() as client:
-            tasks = []
-            for page_num in range(2):
-                url = f'https://www.turbosquid.com/3d-model/sofa?page_num={page_num}&page_size=500&sort_column=a7&sort_order=desc'
-                tasks.append(self.fetch_id(client,url))
-
-            item_ids = await asyncio.gather(*tasks)
-            results = []
-            for group_id in item_ids:
-                results.extend(group_id)
-            return results
 
     async def fetch_detail(self, client, url):
-        response = await client.get(url)
-        json_data = response.json()
-        json_formatted_str = json.dumps(json_data, indent=2)
-        print(json_formatted_str)
-        model_name = json_data['STCPRODUCT']['PRODUCT_NAME']
-        product_id = json_data['STCPRODUCT']['ACTION_HTML'].split('/')[-1]
-        publish_date = ''
-        page_link = json_data['STCPRODUCT']['PRODUCT_LINK']
-        download_link = ''
-        price = HTMLParser(json_data['STCPRODUCT']['PRICE_HTML']).text()
-        model_license = HTMLParser(json_data['STCPRODUCT']['LICENSE_HTML']).css_first('a').text()
-        format = "".join([x.text().strip() for x in HTMLParser(json_data['STCPRODUCT']['PRODUCT_FILES_HTML']).css('li')])
-        polygons = HTMLParser(json_data['STCPRODUCT']['SPECIFICATION_HTML']).css_first('li#preview_details_specification_polygons').text()
-        vertices = HTMLParser(json_data['STCPRODUCT']['SPECIFICATION_HTML']).css_first('li#preview_details_specification_vertices').text()
-        textures = HTMLParser(json_data['STCPRODUCT']['SPECIFICATION_HTML']).css_first('li#preview_details_specification_textures').text()
-        materials = HTMLParser(json_data['STCPRODUCT']['SPECIFICATION_HTML']).css_first('li#preview_details_specification_materials').text()
-        unwarped_uvs = HTMLParser(json_data['STCPRODUCT']['SPECIFICATION_HTML']).css_first('li#preview_details_specification_unwrapped_uvs').text()
-        uv_mapped = HTMLParser(json_data['STCPRODUCT']['SPECIFICATION_HTML']).css_first('li#preview_details_specification_uv_mapped').text()
+        response = await client.get(url, timeout=10.0)
+        return response.json()
 
-        return
 
     async def fetch_all_detail(self, ids):
-        async with httpx.AsyncClient() as client:
-            tasks = []
-            for id in ids[0:2]:
-                url = f'https://www.turbosquid.com/API/v1/Search/Preview/{id}'
-                tasks.append(self.fetch_detail(client,url))
+        proxies = {
+            'http://': f'http://{creds.proxy_username}:{creds.proxy_password}@{creds.proxy_url}:{creds.proxy_port}',
+            'https://': f'http://{creds.proxy_username}:{creds.proxy_password}@{creds.proxy_url}:{creds.proxy_port}'
+        }
 
-            data = await asyncio.gather(*tasks)
-            print(data)
+        urls = [f'https://www.turbosquid.com/API/v1/Search/Preview/{id}' for id in ids if id.isnumeric()]
+        async with httpx.AsyncClient(proxies=proxies) as client:
+            tasks = [asyncio.create_task(self.fetch_detail(client,url)) for url in urls]
+            datas = await asyncio.gather(*tasks, return_exceptions=True)
+            return datas
 
-            # return
+
+    def parse_detail(self, responses):
+        datas = []
+        for response in responses:
+            json_data = response
+            # json_formatted_str = json.dumps(json_data, indent=2)
+            # print(json_formatted_str)
+            model_name = json_data['STCPRODUCT']['PRODUCT_NAME']
+            product_id = HTMLParser(json_data['STCPRODUCT']['ACTION_HTML']).css_first('a').attributes['data-id']
+            publish_date = ''
+            page_link = json_data['STCPRODUCT']['PRODUCT_LINK']
+            download_link = ''
+            price = HTMLParser(json_data['STCPRODUCT']['PRICE_HTML']).text()
+            model_license = HTMLParser(json_data['STCPRODUCT']['LICENSE_HTML']).css_first('a').text()
+            format = ", ".join([re.sub(r'\n                     ', ' | ', x.text().strip()) for x in
+                                HTMLParser(json_data['STCPRODUCT']['PRODUCT_FILES_HTML']).css('li')])
+            polygons = HTMLParser(json_data['STCPRODUCT']['SPECIFICATIONS_HTML']).css_first(
+                'li#preview_details_specification_polygons').text()
+            vertices = HTMLParser(json_data['STCPRODUCT']['SPECIFICATIONS_HTML']).css_first(
+                'li#preview_details_specification_vertices').text()
+            if HTMLParser(json_data['STCPRODUCT']['SPECIFICATIONS_HTML']).css_first(
+                    'li#preview_details_specification_textures'):
+                textures = 'Yes'
+            else:
+                textures = 'No'
+            if HTMLParser(json_data['STCPRODUCT']['SPECIFICATIONS_HTML']).css_first(
+                    'li#preview_details_specification_materials'):
+                materials = 'Yes'
+            else:
+                materials = 'No'
+            if HTMLParser(json_data['STCPRODUCT']['SPECIFICATIONS_HTML']).css_first(
+                    'li#preview_details_specification_unwarped_uvs'):
+                unwarped_uvs = 'Yes'
+            else:
+                unwarped_uvs = 'No'
+            if HTMLParser(json_data['STCPRODUCT']['SPECIFICATIONS_HTML']).css_first(
+                    'li#preview_details_specification_uv_mapped'):
+                uv_mapped = 'Yes'
+            else:
+                uv_mapped = 'No'
+
+            data = asdict(Item(model_name=model_name,
+                                product_id=product_id,
+                                publish_date=publish_date,
+                                page_link=page_link,
+                                download_link=download_link,
+                                price=price,
+                                model_license=model_license,
+                                format=format,
+                                polygons=polygons,
+                                vertices=vertices,
+                                textures=textures,
+                                materials=materials,
+                                unwarped_uvs=unwarped_uvs,
+                                uv_mapped=uv_mapped))
+            datas.append(data)
+        return datas
+
+
+    def to_csv(self, datas, filename):
+        headers = ['model_name', 'product_id', 'publish_date', 'page_link', 'download_link', 'price', 'model_license',
+                   'format', 'polygons', 'vertices', 'textures', 'materials', 'unwarped_uvs', 'uv_mapped']
+        try:
+            for data in datas:
+                try:
+                    file_exists = os.path.isfile(filename)
+                    with open(filename, 'a', encoding='utf-8') as f:
+                        writer = csv.DictWriter(f, delimiter=',', lineterminator='\n', fieldnames=headers)
+                        if not file_exists:
+                            writer.writeheader()
+                        if data != None:
+                            writer.writerow(data)
+                        else:
+                            continue
+                except Exception as e:
+                    print(e)
+                    continue
+        except:
+            pass
+
+
+    async def main(self):
+        keyword = input('What do you want to search? ')
+        print('Getting Product IDs...')
+        last_page = self.get_page(keyword)
+        responses = await self.fetch_all_id(keyword, last_page)
+        ids = s.parse_id(responses)
+        print('Getting Details...')
+        responses = await self.fetch_all_detail(ids)
+        datas = s.parse_detail(responses)
+        print(f'Saving data to {keyword}_result.csv')
+        s.to_csv(datas, f'{keyword}_result.csv')
+        print('Scraping data is done!')
 
 if __name__ == '__main__':
     s=Scraper()
-    ids = asyncio.run(s.fetch_all_id())
-    data = asyncio.run(s.fetch_all_detail(ids))
-#
-#
-# url = f'https://www.turbosquid.com/3d-model/sofa?page_num={page_num}&page_size=500&sort_column=a7&sort_order=desc'
-
-# if __name__ == '__main__':
-#     with httpx.Client() as client:
-#         headers2 = {
-#             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/113.0',
-#             # 'Accept': 'application/json, text/javascript, */*; q=0.01',
-#             # 'Accept-Language': 'en-US,en;q=0.5',
-#             # 'Accept-Encoding': 'gzip, deflate, br',
-#             # 'X-Requested-With': 'XMLHttpRequest',
-#             # 'Connection': 'keep-alive',
-#             # 'Referer': 'https://www.turbosquid.com/3d-model/sofa',
-#             # 'Sec-Fetch-Dest': 'empty',
-#             # 'Sec-Fetch-Mode': 'cors',
-#             # 'Sec-Fetch-Site': 'same-origin'
-#         }
-#         headers = {
-#             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/113.0',
-#             # 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
-#             # 'Accept-Language': 'en-US,en;q=0.5',
-#             # 'Accept-Encoding': 'gzip, deflate, br',
-#             # 'Referer': 'https://www.turbosquid.com/?&utm_source=google&utm_medium=cpc&utm_campaign=RoAsia-en-TS-Brand&utm_content=Brand-TurboSquid&utm_term=turbosquid&mt=e&dev=c&itemid=&targid=kwd-297496938642&loc=9072593&ntwk=g&dmod=&adp=&gclid=CjwKCAjwgqejBhBAEiwAuWHioLyF2JRBqOjUWgfjwDFU8deo7iAObptWjgqW7rtE5dh9uzQ5frw_dRoCFgQQAvD_BwE&gclsrc=aw.ds',
-#             # 'Connection': 'keep-alive',
-#             # 'Upgrade-Insecure-Requests': '1',
-#             # 'Sec-Fetch-Dest': 'document',
-#             # 'Sec-Fetch-Mode': 'navigate',
-#             # 'Sec-Fetch-Site': 'same-origin',
-#             # 'Sec-Fetch-User': '?1'
-#         }
-#         client.get('https://www.turbosquid.com/3d-model/sofa?page_size=500&sort_column=a7&sort_order=desc', headers=headers)
-#         response = client.get('https://www.turbosquid.com/API/v1/Search/Preview/1286600', headers=headers2)
-#         print(response.json())
+    asyncio.run(s.main())
